ade588d189c9c09a6bff086a1a52097881fa5125
LucasZV <l.zamoratech@gmail.com>
2026-05-09 19:22:30 +0200

    Crear rama fonemas

--- Experiments/Phoneme/ConvMambaGRU_Phoneme/train_convmamba_phoneme_model.py
+++
@@ -1,6 +0,0 @@
-from omegaconf import OmegaConf
-from convmamba_phoneme_trainer import BrainToPhonemeDecoderTrainer
-
-args = OmegaConf.load("convmamba_phoneme_args.yaml")
-trainer = BrainToPhonemeDecoderTrainer(args)
-metrics = trainer.train()
--- Experiments/Phoneme/Mamba_Base/train_model.py
+++
@@ -1,6 +0,0 @@
-from omegaconf import OmegaConf
-from mamba_trainer import BrainToTextDecoder_Trainer
-
-args = OmegaConf.load('mamba_args.yaml')
-trainer = BrainToTextDecoder_Trainer(args)
-metrics = trainer.train()
